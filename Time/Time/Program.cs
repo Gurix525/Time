1978bf6e2f1368ee689278c593d8993cb4bea554
Gurix525 <gurix525@gmail.com>
2022-05-11 22:48:24 +0200

    Cleared Program.cs

--- Time/Time/Program.cs
+++ Time/Time/Program.cs
@@ -5,8 +5,6 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.ReadLine();
 
-        Console.WriteLine(new Time("19:59:59") + new Time("12:00:00"));
     }
 }
